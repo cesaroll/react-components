de1d47f9b5e30ffa9c9aceff660c2aae1b50c328
Cesar Lopez <clopezlerma@wayfair.com>
2021-10-21 17:17:25 +0200

    Introduction to HOC (Higher Order Compoenents)

--- src/components/Speaker.tsx
+++ src/components/Speaker.tsx
@@ -118,7 +118,7 @@ const SpeakerDemographics = () => {
       <SpeakerFavorite />
       <div>
         <p className="card-description">
-          {bio.substring(0,70)}
+          {bio}
         </p>
         <div className="social d-flex flex-row mt-4">
           <div className="company">
